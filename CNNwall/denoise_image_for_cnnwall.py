d0dc3f12a8e019ac0e4ab4896d759d98dd6ad9dc
simonlsp <simonlsp@outlook.com>
2021-12-20 22:45:46 +0800

    Modified denoiser reference position for CNNwall.

--- CNNwall/denoise_image_for_cnnwall.py
+++ CNNwall/denoise_image_for_cnnwall.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 import os
 import sys
-from leafnet.leafnet_libs.image_denoiser.StainedDenoiser import denoiser
+from StainedDenoiser import denoiser
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
@@ -13,6 +13,6 @@ current_denoiser = denoiser.denoiser()
 for file_name in os.listdir(input_dir):
     try:
         input_array = np.array(Image.open(os.path.join(input_dir, file_name)).convert('L'))
-        Image.fromarray(current_denoiser.denoise(input_array)).save(os.path.join(input_dir, os.path.splitext(file_name)[0]+".tiff"))
+        Image.fromarray(current_denoiser.denoise(input_array)).save(os.path.join(output_dir, os.path.splitext(file_name)[0]+".tiff"))
     except:
         pass
